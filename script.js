67edaff4ac837331e642b972512fd0d42a4f2bb8
selipearantes <felipearantes.dev@gmail.com>
2024-02-10 13:55:39 -0300

    Alteração - Reconhecer numero maior ou igual a valor maximo parace alert

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min
 
-    alert(result)
-}
+    if (min >= max) {
+         alert('o valor mínimo tem que ser MENOR que o valor máximo!')
+    }
+
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+         alert(result)
+    }}
 
