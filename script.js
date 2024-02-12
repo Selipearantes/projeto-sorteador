16a1a5c82fb3e5a59b3daf8ff3ccaa280a4af525
selipearantes <felipearantes.dev@gmail.com>
2024-02-12 19:13:48 -0300

    algumas alterações no projeto

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 function generateNumber() {
-    const min = Math.ceil(document.querySelector('.input-min').value)
-    const max = Math.floor(document.querySelector('.input-max').value)
+     const min = Math.ceil(document.querySelector('.input-min').value);
+     const max = Math.floor(document.querySelector('.input-max').value);
+     const resultTela = document.querySelector('.resultTela');
 
-    if (min >= max) {
-         alert('o valor mínimo tem que ser MENOR que o valor máximo!')
-    }
-
-    else {
-        const result = Math.floor(Math.random() * (max - min + 1)) + min
-         alert(result)
-    }}
+     if (min >= max) {
+          resultTela.innerHTML = 'o valor mínimo tem que ser MENOR que o valor máximo!';
+          resultTela.style.fontSize = '25px';
+     }
+     else {
+          const result = Math.floor(Math.random() * (max - min + 1)) + min;
+          resultTela.style.fontSize = '55px';
+     
+          resultTela.innerHTML = (result);
+     }
+}
 
